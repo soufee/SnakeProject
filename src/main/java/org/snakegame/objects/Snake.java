4969dfff178137a375381b4601460e6bcb2e7e04
soufee <mutanabbi1>
2017-08-01 16:51:16 +0300

    with scores

--- src/main/java/org/snakegame/objects/Snake.java
+++ src/main/java/org/snakegame/objects/Snake.java
@@ -10,11 +10,10 @@ public class Snake {
     public int direction = 0;
     public int length = 2;
 
-    public int snakeX[] = new int[main.WIDTH*main.HEIGHT];
-    public int snakeY[] = new int [main.WIDTH*main.HEIGHT];
+    public int snakeX[] = new int[main.WIDTH * main.HEIGHT];
+    public int snakeY[] = new int[main.WIDTH * main.HEIGHT];
 
-    public Snake(int x0, int y0, int x1, int y1)
-    {
+    public Snake(int x0, int y0, int x1, int y1) {
         snakeX[0] = x0;
         snakeY[0] = y0;
         snakeX[1] = x1;
@@ -22,27 +21,30 @@ public class Snake {
 
     }
 
-    public void move()
-    {
+    public void move() {
         for (int d = length; d > 0; d--) {
-          snakeX[d]=snakeX[d-1];
-          snakeY[d]=snakeY[d-1];
+            snakeX[d] = snakeX[d - 1];
+            snakeY[d] = snakeY[d - 1];
         }
-        if (direction==0)snakeX[0]++;
-        if (direction==1)snakeY[0]++;
-        if (direction==2)snakeX[0]--;
-        if (direction==3)snakeY[0]--;
+        if (direction == 0) snakeX[0]++;
+        if (direction == 1) snakeY[0]++;
+        if (direction == 2) snakeX[0]--;
+        if (direction == 3) snakeY[0]--;
 
-        for (int d = length-1; d > 0; d--) {
-            if ((snakeX[0]==snakeX[d])&(snakeY[0]==snakeY[d]))length=d-2;
+        for (int d = length - 1; d > 0; d--) {
+            if ((snakeX[0] == snakeX[d]) & (snakeY[0] == snakeY[d])) {
+                length = d - 2;
+                SnakeGame.score = 0;
+            }
         }
 
-        if(snakeX[0]>main.WIDTH-1) snakeX[0]=0;
-        if(snakeX[0]<0) snakeX[0]=main.WIDTH-1;
-        if (snakeY[0]>main.HEIGHT-1) snakeY[0]=0;
-        if(snakeY[0]<0) snakeY[0]=main.HEIGHT-1;
+        if (snakeX[0] > main.WIDTH - 1) snakeX[0] = 0;
+        if (snakeX[0] < 0) snakeX[0] = main.WIDTH - 1;
+        if (snakeY[0] > main.HEIGHT - 1) snakeY[0] = 0;
+        if (snakeY[0] < 0) snakeY[0] = main.HEIGHT - 1;
 
-        if (length<2) length=2;
+        if (length < 2) length = 2;
+
     }
 
 }
